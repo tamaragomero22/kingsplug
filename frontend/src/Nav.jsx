753c03d55543387ce870d9c6eac3e7ebfa5e7ece
Tamara Gomero <tamaragomero22@gmail.com>
2025-10-20 17:27:39 +0100

    About link in the navbar is now link to the features section.

--- frontend/src/Nav.jsx
+++ frontend/src/Nav.jsx
@@ -1,34 +1,54 @@
-import { NavLink, Link } from 'react-router-dom';
-import './Nav.css';
-import homeIcon from './assets/home.svg';
-import logo from './assets/kingsplug.png';
+import { NavLink, Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import "./Nav.css";
+import homeIcon from "./assets/home.svg";
+import logo from "./assets/kingsplug.png";
 
 export const Nav = ({ userEmail, onLogout, logoLinkTo = "/" }) => {
-    return (
-        <header>
-            <div id="headerContainer">
-                <div>
-                    <Link to={logoLinkTo}><img src={logo} alt="Kignsplug logo" /></Link>
-                </div>
+  return (
+    <header>
+      <div id="headerContainer">
+        <div>
+          <Link to={logoLinkTo}>
+            <img src={logo} alt="Kignsplug logo" />
+          </Link>
+        </div>
 
-                <nav>
-                    <ul>
-                        {userEmail ? (
-                            <>
-                                <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-                                <li className="user-email">{userEmail}</li>
-                                <li><button onClick={onLogout} className="nav-logout-btn">Logout</button></li>
-                            </>
-                        ) : (
-                            <>
-                                <li><NavLink to="/"><img src={homeIcon} alt="Home" />Home</NavLink></li>
-                                <li><NavLink to="/about">About</NavLink></li>
-                                <li><NavLink to="/register">Sign up</NavLink></li>
-                            </>
-                        )}
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
-}
+        <nav>
+          <ul>
+            {userEmail ? (
+              <>
+                <li>
+                  <NavLink to="/dashboard">Dashboard</NavLink>
+                </li>
+                <li className="user-email">{userEmail}</li>
+                <li>
+                  <button onClick={onLogout} className="nav-logout-btn">
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="/">
+                    <img src={homeIcon} alt="Home" />
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <HashLink smooth to="/#features">
+                    About
+                  </HashLink>
+                </li>
+                <li>
+                  <NavLink to="/register">Sign up</NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
