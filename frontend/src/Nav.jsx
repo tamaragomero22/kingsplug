2d6e24dd0b79382914cdd3c21d97d7db9efe25d6
Tamara Gomero <tamaragomero22@gmail.com>
2026-02-18 18:15:45 +0100

    Modified logo

--- frontend/src/Nav.jsx
+++ frontend/src/Nav.jsx
@@ -3,7 +3,7 @@ import { HashLink } from "react-router-hash-link";
 import { useState } from "react";
 import "./Nav.css";
 import homeIcon from "./assets/home.svg";
-import logo from "./assets/Kingsplug.png";
+import logo from "./assets/kingsplug.png";
 
 export const Nav = ({
   userEmail,
